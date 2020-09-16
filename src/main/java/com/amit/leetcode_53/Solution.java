7b5aec32cc8bc2264d23a26793fde6987775040e
Amit Mondal <amit.kuet@gmail.com>
2020-09-16 13:53:54 +0200

    Solutions with Java

---
+++ src/main/java/com/amit/leetcode_53/Solution.java
@@ -0,0 +1,27 @@
+package com.amit.leetcode_53;
+
+/**
+ * 
+ * Time - O(N) single pass
+ * Space - O(1)
+ *
+ */
+public class Solution {
+
+	public int maxSubArray(int[] nums) {
+		int maxSoFar = nums[0];
+		int maxEndingHere = nums[0];
+
+		for (int i = 1; i < nums.length; i++) {
+			maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+			maxSoFar = Math.max(maxSoFar, maxEndingHere);
+		}
+		return maxSoFar;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(new Solution().maxSubArray(nums)); // Output is 6
+	}
+
+}
